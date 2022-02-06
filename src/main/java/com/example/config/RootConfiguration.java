1001bee8b72145f73ddcbfcf4c88d50301ab4814
wanliu <1692894189@qq.com>
2022-02-06 22:03:48 +0800

    使用数据库认证

--- src/main/java/com/example/config/RootConfiguration.java
+++ src/main/java/com/example/config/RootConfiguration.java
@@ -1,12 +1,36 @@
 package com.example.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScans(
+import javax.sql.DataSource;
+
+@ComponentScans({
         @ComponentScan("com.example.service")
-)
+})
+@MapperScan("com.example.mapper")
+@Configuration
 public class RootConfiguration {
+    @Bean
+    public DataSource dataSource(){
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/book2");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(@Autowired DataSource dataSource){
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        return bean;
+    }
 }
--- src/main/java/com/example/config/SecurityConfiguration.java
+++ src/main/java/com/example/config/SecurityConfiguration.java
@@ -1,11 +1,26 @@
 package com.example.config;
 
+import com.example.service.UserAuthService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.Resource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     //继承WebSecurityConfigurerAdapter，之后会进行配置
+
+    @Resource
+    UserAuthService service;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(service)   //使用自定义的Service实现类进行验证
+                .passwordEncoder(new BCryptPasswordEncoder());   //依然使用BCryptPasswordEncoder
+    }
 }
